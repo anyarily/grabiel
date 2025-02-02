32872f98e2a142a3abf2efca01f58edaf5f80832
anyarily <amyarilisperez0414@gmail.com>
2025-02-02 16:02:39 -0400

    cabeza

--- pages/forder/Gabriel.jsx
+++ pages/forder/Gabriel.jsx
@@ -5,6 +5,8 @@ export default function Gabriel(){
 
 return <div>
     <h3>enemigo</h3>
+    <h2>amigos nuevog</h2>
+    <h1>nos vamos tos</h1>
 <button  onClick={function(){ alert("hola amhjbkmnilhcogoljh hjk adduhjbs")}}  >
     probar
 </button>
